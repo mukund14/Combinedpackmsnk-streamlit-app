be0894f6ed26450f4a3e1fba7b164fff4d20f95c
Mukundan Sankar <mukund14@users.noreply.github.com>
2024-06-04 23:46:20 -0700

    Delete combinedpackmsnk.py

--- combinedpackmsnk.py
+++
@@ -1,8 +0,0 @@
-def func(df):
-    # Assuming df is already a pandas DataFrame
-    # Process the DataFrame as needed
-    print(df.head(3))
-    # Perform the rest of the data processing
-    # ...
-
-    return {"message": "Processing complete", "data": df.describe().to_dict()}
